0a5b65182985baa2226947f2a0a2a05a0b677f11
Ambasodor <31244488+Ambasodor@users.noreply.github.com>
2021-05-15 17:12:44 +0300

    -- Перерисовали fight UI
    -- Перерисовали fight UI
    -- Кнопка Z(по дефалту) - отмечает текущую цель для группы
    -- Кнопка Shift+Space(по дефалту) - фокусируется по таргету
    -- Добавил настройку "Быстрое действие" (кнопка Q)
    -- Добавил "Быстрое действие" возле мышки - (Alt+Q)
    -- Некоторые фиксы

--- src/haven/GameUI.java
+++ src/haven/GameUI.java
@@ -32,6 +32,7 @@ import haven.automation.PickForageable;
 import haven.automation.Traverse;
 import haven.automation.GoIn;
 import haven.overlays.OverlayManager;
+import haven.sloth.gob.AggroMark;
 import haven.purus.DrinkWater;
 import haven.purus.ItemClickCallback;
 import haven.purus.pbot.PBotScriptlist;
@@ -77,6 +78,7 @@ import static haven.Action.TOGGLE_KIN_LIST;
 import static haven.Action.TOGGLE_OPTIONS;
 import static haven.Action.TOGGLE_SEARCH;
 import static haven.KeyBinder.KeyBind;
+import static haven.MCache.tilesz;
 
 public class GameUI extends ConsoleHost implements Console.Directory {
     public static final Text.Foundry msgfoundry = new Text.Foundry(Text.dfont, Text.cfg.msg);
@@ -495,11 +497,22 @@ public class GameUI extends ConsoleHost implements Console.Directory {
             if (fv != null && fv.current != null)
                 g = map.glob.oc.getgob(fv.current.gobid);
             if (g != null) {
-                g.mark(20000);
+                synchronized (fv.lsrel) {
+                    for (Fightview.Relation rel : fv.lsrel) {
+                        final Gob gob = ui.sess.glob.oc.getgob(rel.gobid);
+                        if (gob != null) {
+                            final Gob.Overlay ol = gob.findol(AggroMark.id);
+                            if (ol != null) {
+                                gob.ols.remove(ol);
+                            }
+                        }
+                    }
+                }
+                g.aggromark(10000);
                 for (Widget wdg = chat.lchild; wdg != null; wdg = wdg.prev) {
                     if (wdg instanceof ChatUI.PartyChat) {
                         final ChatUI.PartyChat chat = (ChatUI.PartyChat) wdg;
-                        chat.send(String.format(Mark.CHAT_FMT, g.id, 20000));
+                        chat.send(String.format(Mark.CHAT_FMT, g.id, 10000));
                     }
                 }
             }
@@ -1601,6 +1614,27 @@ public class GameUI extends ConsoleHost implements Console.Directory {
         }
     }
 
+    public void switchmarkedtarget() {
+        Fightview.Relation cur = fv.current;
+        if (cur != null) {
+            synchronized (fv.lsrel) {
+                for (Fightview.Relation rel : fv.lsrel) {
+                    final Gob gob = ui.sess.glob.oc.getgob(rel.gobid);
+                    if (gob != null) {
+                        final Gob.Overlay ol = gob.findol(AggroMark.id);
+                        if (ol != null) {
+                            fv.lsrel.remove(cur);
+                            fv.lsrel.addLast(cur);
+                            fv.wdgmsg("bump", (int) gob.id);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
     public void toggleGobs() {
         Config.showboundingboxes = !Config.showboundingboxes;
         Utils.setprefb("showboundingboxes", Config.showboundingboxes);
@@ -1810,6 +1844,13 @@ public class GameUI extends ConsoleHost implements Console.Directory {
     public void harvestForageable() {
         Thread t = new Thread(new PickForageable(this), "PickForageable");
         t.start();
+    }
+
+    public void harvestMForageable() {
+        if (map != null && map.lastmovemc != null) {
+            Thread t = new Thread(new PickForageable(this, map.lastmovemc), "PickMForageable");
+            t.start();
+        }
     }
 
     public void traverse() {
